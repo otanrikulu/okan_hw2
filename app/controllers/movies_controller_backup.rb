4b65fdc995e3df3c08d5055ccab0e2a64d03a75b
“Okan <motanrikulu@gmail.com>
2012-10-19 15:35:43 -0500

    HW2 - Part 2

--- app/controllers/movies_controller_backup.rb
+++ app/controllers/movies_controller_backup.rb
@@ -6,51 +6,48 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
-  def index
 
+
+  def index
 #	@all_ratings = Movie.all(:select => 'rating').map(&:rating).uniq
 #	This is working. Now, let's define a method in Movie.
 
 	sort = params[:sort]
 	@all_ratings = Movie.ratingcollect #reads all available ratings from the table
-#	@ratingselection = params[:ratings] || session[:ratings] || {}
-#	@ratingselection = @all_ratings
+	@ratingselection = params[:ratings] || session[:ratings] || {}
 	@my_session_sort = session[:sort]
-	@my_params_sort = params[:sort]
 	@my_session_rating = session[:ratings]
-	@my_params_rating = params[:ratings]
+
 
 #	if @ratingselection == {} then
 #		@ratingselection = @all_ratings
 #	else @ratingselection = params[:ratings]
 #	end
-
-	@ratingselection = params[:ratings] || @all_ratings
-#	@ratingselection = params[:ratings] || session[:ratings] || {}
-#	if @ratingselection == (params[:ratings] || session[:ratings] || {}) then
-#		@ratingsselection
-#	else
-#		@ratingselection = @all_ratings
-#	end
-
+	
 	case sort
 	when 'title'
-#		@checked_ratings = @ratingselection.keys
-#		@movies = Movie.find(:all, :order => 'title')
-		@movies = Movie.find_all_by_rating(@ratingselection, :order => 'title')
-#		@movies = Movie.find_all_by_rating(@checked_ratings, :order => 'title')
+		@movies = Movie.find(:all, :order => 'title')
+#		@movies = Movie.find_all_by_rating(@ratingselection, :order => 'title')
 		@title_header = {:order => :title}, 'hilite'
 	when 'release_date'
-#		@checked_ratings = @ratingselection.keys
-#		@movies = Movie.find(:all, :order => 'release_date')
-		@movies = Movie.find_all_by_rating(@ratingselection, :order => 'release_date')
-#		@movies = Movie.find_all_by_rating(@checked_ratings, :order => 'release_date')
+		@movies = Movie.find(:all, :order => 'release_date')
+#		@movies = Movie.find_all_by_rating(@ratingselection, :order => 'release_date')
 		@release_date_header = {:order => :release_date}, 'hilite'
 	else
-		@checked_ratings = @ratingselection.keys
-		@movies = Movie.find_all_by_rating(@checked_ratings)
-#		@movies = Movie.find_all_by_rating(@ratingselection)
+		@movies = Movie.all
 	end
+
+#	if params[:sort] != session[:sort]
+#		session[:sort] = sort
+#		redirect_to :sort => sort, :ratings => @ratingselection and return
+#	end	
+#	if params[:ratings] != session[:ratings] and @ratingselection != {}
+#      		session[:sort] = sort
+#      		session[:ratings] = @ratingselection
+#      		redirect_to :sort => sort, :ratings => @ratingselection and return
+#	end	
+#	@movies = Movie.find_all_by_rating(@ratingselection.keys, Movie.find(:all, :order => 'title'))
+
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  def self.ratingcollect
+	@all_ratings = Movie.all(:select => 'rating').map(&:rating).uniq
+  end
 end

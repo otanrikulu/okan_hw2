44ac9b440e99bee8e08c4afa598d2d0598a94e67
“Okan <motanrikulu@gmail.com>
2012-10-20 14:51:13 -0500

    Retry with the old version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,41 +8,58 @@ class MoviesController < ApplicationController
 
   def index
 
-    sort = params[:sort] || session[:sort]
-    case sort
-    when 'title'
-      @movies = Movie.find_all_by_rating(@selected_ratings, :order => 'title')
-      @title_header = {:order => :title}, 'hilite'
-    when 'release_date'
-      @movies = Movie.find_all_by_rating(@selected_ratings, :order => 'release_date')
-      @release_date_header = {:order => :release_date}, 'hilite'
-    end
-#    @all_ratings = Movie.ratingcollect
-    @all_ratings = ["G", "R", "PG-13", "PG"]
-#    @all_ratings = %w(G PG PG-13 NC-17 R)
-#    all_ratings_hash = Hash.new()
-#    c = 0
-#      while c < @all_ratings.size
-#      all_ratings_hash.store(@all_ratings[c], "1")
-#      c += 1
-#    end
+#    @all_ratings = Movie.all(:select => 'rating').map(&:rating).uniq
+#    This is working. Now, let's define a method in Movie.
 
-    my_ratings = Hash.new()
-    all_ratings_hash = Utils.ratinghash(@all_ratings)
-#    all_ratings_hash = {"G"=>"1", "R"=>"1", "PG-13"=>"1", "PG"=>"1"}
-    @selected_ratings = params[:ratings] || session[:ratings] || all_ratings_hash
+    @sortorder = params[:sort] || session[:sort]
+    @all_ratings = Movie.ratingcollect #reads all available ratings from the table
 
-    if params[:sort] != session[:sort]
-      session[:sort] = sort
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
-    end
+    @my_session_sort = session[:sort]
+    @my_params_sort = params[:sort]
+    @my_session_rating = session[:ratings]
+    @my_params_rating = params[:ratings]
 
-    if params[:ratings] != session[:ratings] and @selected_ratings != {}
-      session[:sort] = sort
-      session[:ratings] = @selected_ratings
-      redirect_to :sort => sort, :ratings => @selected_ratings and return
-    end
-    @movies = Movie.find_all_by_rating(@selected_ratings.keys,:order => sort)
+# Check out Hash to Hash assignments...
+    @ratingselection = params[:ratings] || @all_ratings
+
+	if @ratingselection == @all_ratings then
+		@all_ratings_h = Hash.new{}
+		c = 0
+		while c < @all_ratings.size
+		@all_ratings_h.store(@all_ratings[c], "1")
+		c += 1
+		end
+		@ratingselection = @all_ratings_h
+	
+	end
+
+#	@movies = Movie.find(:all)
+	case @sortorder
+	when 'title'
+#		@checked_ratings = @ratingselection.keys
+#		@movies = Movie.find(:all, :order => 'title')
+		@okan = 11111
+		@movies = Movie.find_all_by_rating(@ratingselection.keys, :order => 'title')
+		@title_header = {:order => :title}, 'hilite'
+	when 'release_date'
+#		@checked_ratings = @ratingselection.keys
+#		@movies = Movie.find(:all, :order => 'release_date')
+		@okan = 22222
+		@movies = Movie.find_all_by_rating(@ratingselection.keys, :order => 'release_date')
+		@release_date_header = {:order => :release_date}, 'hilite'
+#	else
+#		@checked_ratings = @ratingselection
+#		@movies = Movie.find_all_by_rating(@all_ratings_h.keys)
+#		@movies = Movie.find_all_by_rating(@ratingselection.keys)
+	end
+	
+	if params[:ratings] != session[:ratings] and @ratingselection != {}
+		session[:sort] = @sortorder
+		session[:ratings] = @ratingselection
+		redirect_to :sort => @sortorder, :ratings => @ratingselection and return
+	end
+#	@movies = Movie.find_all_by_rating(@ratingselection)
+
 
   end
 
